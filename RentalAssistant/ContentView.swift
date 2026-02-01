32beb857c0e742bcc73b9fce53700b6584bf6a59
Vamsi Valluri <vvalluri@gmail.com>
2026-01-31 21:56:24 -0800

    Initial Commit

---
+++ RentalAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RentalAssistant
+//
+//  Created by Vamsidhar Valluri on 1/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RentalAssistant/RentalAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentalAssistantApp.swift
+//  RentalAssistant
+//
+//  Created by Vamsidhar Valluri on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct RentalAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
